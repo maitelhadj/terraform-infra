a26f26d784eac74e18d1e70344cdfd5e5408e314
Mayas <mayasaeh213@gmail.com>
2023-01-12 12:05:55 +0100

    test

--- output.tf
+++ output.tf
@@ -1,8 +1,10 @@
 output "instance_ip_addr" {
-    value = [{
+    for_each = [{
         for k, instance in scaleway_instance_server.server : k => instance.private_ip
     },
     {
         for k, instance in scaleway_instance_server.server : k => instance.public_ip
     }]
+
+    value = "${each.key} = ${each.value}"
 }
