480853ea942c9998517b4242ca736314ecf7dc5b
Mayas <mayasaeh213@gmail.com>
2023-01-12 12:03:21 +0100

    Test

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "instance_ip_addr" {
     value = {
-        for k, instance in scaleway_instance_server.server : k => instance.private_ip
+        for k, instance in scaleway_instance_server.server
     }
 }
