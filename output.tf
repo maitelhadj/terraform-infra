fc6640ef803559ebcc362ec412bdb7070c4c557e
Mayas <mayasaeh213@gmail.com>
2023-01-12 12:01:24 +0100

    out correctif

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "instance_ip_addr" {
-    count = var.instance_count
-    value = "\nServeur ${scaleway_instance_server[count.index].name} :\n\tPrivate IP : ${scaleway_instance_server[count.index].private_ip}\n\tPublic IP : ${scaleway_instance_server[count.index].public_ip}\n"
+    value = {
+        for k, instance in scaleway_instance_server.server : k => instance.private_ip
+    }
 }
