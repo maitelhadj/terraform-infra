404aedce1aebd397caf2845c224ed0d9ce787c42
Mayas <mayasaeh213@gmail.com>
2023-01-12 12:12:12 +0100

    maj output

--- output.tf
+++ output.tf
@@ -1,4 +1,12 @@
-output "instance_ip_addr" {
+output "instance_public_ip_addr" {
+    description = "Instance's public IP"
+
+    value = {
+        for instance in scaleway_instance_server.server : instance.name => instance.public_ip
+    }
+}
+
+output "instance_private_ip_addr" {
     description = "Instance's private IP"
 
     value = {
