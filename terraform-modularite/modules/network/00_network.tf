3d46d6f6d5e12981c034de8be9c5ecb1bea06946
Mayas <mayasaeh213@gmail.com>
2023-01-12 17:06:29 +0100

    modularite on the road

---
+++ terraform-modularite/modules/network/00_network.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+      version = "~> 2.8.0"
+    }
+  }
+  required_version = "~> 1.3.0"
+}
+
+resource "scaleway_vpc_private_network" "private_network" {
+  name = "${var.prefix}-private-network"
+}
+
+resource "scaleway_instance_ip" "public_ip" {}
