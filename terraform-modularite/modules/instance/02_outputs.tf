24d6027c183afafe0c130235906a0fa590b68647
Mayas <mayasaeh213@gmail.com>
2023-01-13 15:39:58 +0100

    facto

--- terraform-modularite/modules/instance/02_outputs.tf
+++ terraform-modularite/modules/instance/02_outputs.tf
@@ -1,7 +1,7 @@
-output "server" {
+output "server_id" {
     value = scaleway_instance_server.server.id
 }
 
-output "public_ip_ese1" {
+output "public_ip" {
     value = scaleway_instance_server.server.public_ip
 }
