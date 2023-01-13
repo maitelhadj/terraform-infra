b5167bdaacff260f1d611ffc3963db88d1a05a31
Mayas <mayasaeh213@gmail.com>
2023-01-13 16:57:21 +0100

    docu

--- terraform-modularite/00_main.tf
+++ terraform-modularite/00_main.tf
@@ -8,6 +8,14 @@ terraform {
   required_version = "~> 1.3.0"
 }
 
+# Un module n'est qu'un bout de recette, par exemple je peux créer un module dans lequel je vais déployer plusieurs ressources
+# tel que : une public_ip, une instance...
+#
+# On fait un appel au module network dans notre recette principale :
+#   - source est le chemin vers le module que je souhaite utiliser (on va y retrouver les fichiers classique d'une recette terraform, un main.tf, un variables.tf et outputs.tf)
+#   - les arguments "prefix" et "port" sont des variables qui sont déclarées dans le fichier "modules/network/01_variables.tf" et que je peux initialiser directement dans ce bloc module
+# 
+# Si je fais un terraform apply sur cette recette (le main dans lequel on se trouve), ce dernier va faire un appel au module network et appliquer la recette que tu auras défini dans ce module
 module "network_module" {  
   source = "./modules/network"
   
@@ -17,6 +25,16 @@ module "network_module" {
   port   = var.port
 }
 
+# Ici toujours un module <==
+# La particularité est que j'utilise un for_each pour pouvoir appeler ce module plusieurs fois
+# 
+# De plus, on note l'utilisation de la syntaxe "module.network_module.app_sg_id". Cela me permet de récupérer une information d'un autre module.
+# En l'occurence, j'accède au module "network_module" créé précédemment, en particulier, j'accède à l'output "app_sg_id" que j'ai créé ici => ""./modules/network/02_outputs.tf"
+# C'est ainsi que l'on peut transférer des valeurs d'un module à un autre.
+#
+# Grosso modo, les variables sont les "inputs", le main est le "processor" et outputs n'est rien d'autre que les "outputs"
+# 
+# Petit lien vers la doc : https://developer.hashicorp.com/terraform/language/meta-arguments/for_each
 module "instance_module" {
   for_each = var.instances
 
@@ -33,6 +51,8 @@ module "instance_module" {
   depends_on = [module.network_module]
 }
 
+# Ici toujours un module <==
+# Ce module deploie docker sur les instance créées précédemment
 module "provision_module" {
   for_each = module.instance_module
 
